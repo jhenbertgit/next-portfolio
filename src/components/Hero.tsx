03a2e5ff61196f91bd9f18dc3a0beefeda12b874
Jhenbert <jvv202726@gmail.com>
2024-12-22 17:48:25 +0800

    chore: minor change

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -58,6 +58,7 @@ const Hero = () => {
             </a>
           </div>
         </motion.div>
+
         <motion.div
           initial={{ opacity: 0, scale: 0.5 }}
           animate={{ opacity: 1, scale: 1 }}
