91b5c455fc43e70accb113581564435e29917cfc
Jhenbert <jvv202726@gmail.com>
2024-12-23 22:29:06 +0800

    fix: static export output dir set to out

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // Enable static exports
+  distDir: "out", // Set output directory to 'out'
+  // ... your other config options
 };
 
 export default nextConfig;
