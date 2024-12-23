6fea17ffec96c290fa02ba396df8f85663a63264
Jhenbert <jvv202726@gmail.com>
2024-12-23 23:07:56 +0800

    fix: images unoptimize to true

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "export", // Enable static exports
   distDir: "out", // Set output directory to 'out'
-  // ... your other config options
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
