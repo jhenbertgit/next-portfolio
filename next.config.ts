28587cc680461f2e1d35bbfd0aea02b12a403a28
Jhenbert <jvv205756@gmail.com>
2025-04-23 20:42:32 +0800

    Disable static page build

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export", // Enable static exports
-  distDir: "out", // Set output directory to 'out'
-  images: {
-    unoptimized: true,
-  },
+  // output: "export", // Enable static exports
+  // distDir: "out", // Set output directory to 'out'
+  // images: {
+  //   unoptimized: true,
+  // },
 };
 
 export default nextConfig;
