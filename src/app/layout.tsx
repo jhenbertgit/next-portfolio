cc8a4f91db814522bcb974ceba54da92b8dc6726
Jhenbert <jvv205756@gmail.com>
2025-05-05 11:51:11 +0800

    Added vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Toaster } from "@/components/ui/toaster";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -64,6 +65,7 @@ export default function RootLayout({
         {children}
 
         <Toaster />
+        <Analytics />
       </body>
     </html>
   );
