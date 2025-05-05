fc8690d9872e4171ab42120a985c0a2834759344
Jhenbert <jvv205756@gmail.com>
2025-05-05 12:03:00 +0800

    Added vercel speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Toaster } from "@/components/ui/toaster";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 import "./globals.css";
 
 const geistSans = Geist({
@@ -66,6 +68,7 @@ export default function RootLayout({
 
         <Toaster />
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
